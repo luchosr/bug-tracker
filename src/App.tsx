b7f232c4ab9018a0e7075556e1b35fd1c4a0d9e4
luchosr <luchosr@gmail.com>
2021-07-29 12:42:05 +0200

    added ui elements

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { IBug } from "./IBug";
 
 function App() {
-  const [debugDescription, setDebugDescription] = useState<string>("");
+  const [newBugDescription, setNewBugDescription] = useState<string>("");
   const [newBugPriority, setNewBugPriority] = useState<string>("Medium");
   const [bugList, setBugList] = useState<IBug[]>([]);
 
@@ -14,7 +14,23 @@ function App() {
     <div>
       <h1>🐞 Bug Tracker</h1>
       <form onSubmit={addBug}>
-        <label htmlFor="newBugDescription"></label>
+        <label htmlFor="newBugDescription">New bug Description:</label>
+        <input
+          type="text"
+          id="newBugDescription"
+          value={newBugDescription}
+          onChange={(event) => setNewBugDescription(event.target.value)}
+        />
+        <label htmlFor="newBugPriority">New Bug Priority</label>
+        <select
+          id="newBugPriority"
+          value={newBugPriority}
+          onChange={(event) => setNewBugPriority(event.target.value)}
+        >
+          <option value="Low">Low</option>
+          <option value="Medium">Medium</option>
+          <option value="Hight">Hight</option>
+        </select>
       </form>
     </div>
   );
